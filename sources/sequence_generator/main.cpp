b45e16e1c418118c3c4ff402a246a398eefab722
Miloš Šimek <tjsnake@me.com>
2016-12-21 14:27:27 +0100

    Adds sequence generating algorithm (sequence_generator)

--- sources/sequence_generator/main.cpp
+++ sources/sequence_generator/main.cpp
@@ -8,18 +8,41 @@
 //
 
 #include <iostream>
+#include <random>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 #include "SeqGenOptions.hpp"
 #include "ProbMatrixLoader.hpp"
 #include "../global/N44Matrix.hpp"
 #include "../global/Fasta.hpp"
 using namespace std;
+typedef unsigned long long ULL;
 
 
+struct Sequence {
+    string sequence;
+    unsigned fragmentIndex;
+    ULL position;
+};
+
+
+//function declarations
 N44Matrix getMatrix(SeqGenOptions& options);
 N44Matrix generateUniformProb(float uniformProbability, unsigned matrixLength);
 void loadGenome(Fasta& genome, SeqGenOptions& options);
 void testOutputFiles(ofstream& seqFile, ofstream& mapFile);
+
+vector<Sequence> generateSequences(Fasta& genome,
+                                   float coverage,
+                                   unsigned seqLength,
+                                   mt19937_64& randGen);
+void generateErrors(vector<Sequence>& sequences,
+                    N44Matrix probMatrix,
+                    mt19937_64& randGen);
+void saveSequences(vector<Sequence>& sequences, ofstream& seqFile);
+void saveMapInfo(vector<Sequence>& sequences, ofstream& mapFile);
 
 
 
@@ -31,6 +54,7 @@ int main(int argc, const char * argv[]) {
     if (options.optionsState() == OPS_ERR)  return 1;
     
     //load input data
+    cout << "Loading imput files" << endl;
     N44Matrix probMatrix = getMatrix(options);
     Fasta genome;
     loadGenome(genome, options);
@@ -40,23 +64,34 @@ int main(int argc, const char * argv[]) {
     ofstream outputMapFile(options.getOutputMapFileName());
     testOutputFiles(outputSeqFile, outputMapFile);
     
-    //setup random generator seed
-    unsigned randGenSeed;
-    if (options.randGenSeed()) randGenSeed = *options.randGenSeed();
-    else randGenSeed = static_cast<unsigned>(time(nullptr));
+    //setup random generator
+    mt19937_64 randGenerator;
+    if (options.randGenSeed()) randGenerator.seed(*options.randGenSeed());
+    else randGenerator.seed(static_cast<unsigned>(time(nullptr)));
     
     //generate sequences
-    
+    cout << "Generating sequences" << endl;
+    vector<Sequence> sequences = generateSequences(genome,
+                                                   options.getCoverage(),
+                                                   options.getSeqLength(),
+                                                   randGenerator);
+    cout << "    " << sequences.size() << " sequences generated" << endl;
+ 
     //introduce errors
+    cout << "Introducing errors" << endl;
+    generateErrors(sequences, probMatrix, randGenerator);
     
     //shuffle sequences
+    cout << "Shuffling" << endl;
+    shuffle(sequences.begin(), sequences.end(), randGenerator);
     
     //safe to files
+    cout << "Saving" << endl;
+    saveSequences(sequences, outputSeqFile);
+    saveMapInfo(sequences, outputMapFile);
     
-    
-    //algoritmus generování sequencí musí počítat s tím
-    //že fragment může být kratší než sekvence (i délky 0)
-    //v té chvíli by měl algoritmus daný fragment ignorovat
+    cout << "Done" << endl;
+    cout << endl;
     
 }
 
@@ -126,3 +161,103 @@ void testOutputFiles(ofstream& seqFile, ofstream& mapFile) {
         exit(1);
     }
 }
+
+vector<Sequence> generateSequences(Fasta& genome,
+                                   float coverage,
+                                   unsigned seqLength,
+                                   mt19937_64& randGen) {
+    vector<Sequence> sequences;
+    
+    for (unsigned fragIndex = 0; fragIndex < genome.getFragmentCount(); fragIndex++) {
+        string& fragment = genome.getData(fragIndex);
+        
+        //ignoring fragments shorter than sequence
+        if (fragment.length() < seqLength) continue;
+        
+        //compute number of sequences from coverage
+        ULL seqCount = round((fragment.length() / seqLength) * coverage);
+        
+        size_t maxPosition = fragment.length() - seqLength;
+        
+        //generate sequences
+        for (ULL seqNum = 0; seqNum < seqCount; seqNum++) {
+            size_t position = randGen() % (maxPosition + 1);
+            
+            Sequence seq;
+            seq.sequence = fragment.substr(position, seqLength);
+            seq.fragmentIndex = fragIndex;
+            seq.position = position;
+            
+            sequences.push_back(seq);
+        }
+    }
+    
+    return sequences;
+}
+
+
+#define END_A probA
+#define END_C probA + probC
+#define END_G probA + probC + probG
+
+void generateErrors(vector<Sequence>& sequences,
+                    N44Matrix probMatrix,
+                    mt19937_64& randGen) {
+    
+    for (Sequence& seq: sequences) {
+        string& sequence = seq.sequence;
+        
+        for (unsigned seqPos = 0; seqPos < sequence.length(); seqPos++) {
+            char& base = sequence[seqPos];
+            if (base == 'N') continue;
+            
+            //get row of probability matrix
+            char row = 0;
+            switch (base) {
+                case 'A': case 'a': row = 0; break;
+                case 'C': case 'c': row = 1; break;
+                case 'G': case 'g': row = 2; break;
+                case 'T': case 't': row = 3; break;
+                    
+                default: throw logic_error("bad_base"); break;
+            }
+            
+            //get probabilities as whole numbers
+            unsigned probA = probMatrix(seqPos, row, 0) * 10000;
+            unsigned probC = probMatrix(seqPos, row, 1) * 10000;
+            unsigned probG = probMatrix(seqPos, row, 2) * 10000;
+            unsigned probT = probMatrix(seqPos, row, 3) * 10000;
+            
+            unsigned fulProbabilityNum = probA + probC + probG + probT;
+            unsigned randNumber = randGen() % fulProbabilityNum;
+            
+            //put errors in sequence
+            if (randNumber <= END_A) base = 'A';
+            if (randNumber > END_A && randNumber <= END_C) base = 'C';
+            if (randNumber > END_C && randNumber <= END_G) base = 'G';
+            if (randNumber > END_G)  base = 'T';
+        }
+    }
+}
+
+void saveSequences(vector<Sequence>& sequences, ofstream& seqFile) {
+    for (ULL index = 0; index < sequences.size(); index++) {
+        string fastqEntry;
+        
+        fastqEntry += string("@") + to_string(index) + "\n";
+        fastqEntry += sequences[index].sequence + "\n";
+        fastqEntry += "+\n";
+        fastqEntry += string(sequences[index].sequence.length(), '!') + "\n";
+        
+        seqFile << fastqEntry;
+    }
+}
+
+void saveMapInfo(vector<Sequence>& sequences, ofstream& mapFile) {
+    for (ULL index = 0; index < sequences.size(); index++) {
+        Sequence& seq = sequences[index];
+        
+        mapFile << seq.fragmentIndex << " " << seq.position << "\n";
+    }
+}
+
