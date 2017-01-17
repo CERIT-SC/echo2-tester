f61997dcdc64c5a1201064250c464a76295e42d1
Miloš Šimek <tjsnake@me.com>
2017-01-17 10:01:07 +0100

    Adds project samtomap

---
+++ sources/samtomap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  samtomap
+//
+//  Created by Miloš Šimek on 17/01/2017.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
