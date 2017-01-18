ca87ef34c51ed083669e142d90e4548d783fd48d
Miloš Šimek <tjsnake@me.com>
2017-01-19 00:58:45 +0100

    Adds options declaration (samtomap)

--- sources/samtomap/SamToMapOptions.hpp
+++ sources/samtomap/SamToMapOptions.hpp
@@ -9,6 +9,28 @@
 #ifndef SamToMapOptions_hpp
 #define SamToMapOptions_hpp
 
+#include <string>
+#include <boost/program_options.hpp>
 
+using namespace std;
+namespace po = boost::program_options;
+
+enum OptionsState {
+    OPS_OK, OPS_HELP, OPS_ERR
+};
+
+class SamToMapOptions {
+    OptionsState opState = OPS_OK;
+    po::variables_map optionMap;
+    
+public:
+    SamToMapOptions(int argc, const char * argv[]);
+    
+    string getSamFileName();
+    string getGenomeFileName();
+    string getMapFileName();
+    
+    OptionsState optionsState();
+};
 
 #endif /* SamToMapOptions_hpp */
