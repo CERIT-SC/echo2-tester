4f80a00c49bf5f942840c554f4d0460e60368bdd
Miloš Šimek <tjsnake@me.com>
2017-01-19 15:02:45 +0100

    Adds loading files (samtomap)

--- sources/samtomap/main.cpp
+++ sources/samtomap/main.cpp
@@ -7,10 +7,50 @@
 //
 
 #include <iostream>
+#include <fstream>
 
 #include "SamToMapOptions.hpp"
+#include "../global/Fasta.hpp"
 
 int main(int argc, const char * argv[]) {
+    
+    //load options
+    SamToMapOptions options(argc, argv);
+    if (options.optionsState() == OPS_HELP) return 0;
+    if (options.optionsState() == OPS_ERR)  return 1;
+    
+    cout << "Loading files" << endl;
+    
+    //open sam file
+    ifstream samFile(options.getSamFileName());
+    if (!samFile.is_open()) {
+        cerr << "Cannot open sam file" << endl << endl;
+        return 1;
+    }
+    
+    //load genome
+    Fasta genome;
+    try {
+        genome.loadFromFile(options.getGenomeFileName());
+    } catch (exception &e) {
+        if (string(e.what()) == "cannot-open-file") {
+            cerr << "Cannot open genome file" << endl << endl;
+            return 1;
+        }
+        
+        cerr << "Problem while loading genome file" << endl << endl;
+        return 1;
+    }
+    
+    //open map file for writing
+    ofstream mapFile(options.getMapFileName());
+    if (!mapFile.is_open()) {
+        cerr << "Cannot create/open map file for writing" << endl << endl;
+        return 1;
+    }
+    
+    
+    
     
     //vstup: sam soubor, genome fasta soubor
     //výstup: map soubor
